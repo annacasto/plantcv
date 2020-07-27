529fa47b2c945657025b67122b9ae70c1560dffb
annacasto <61164490+annacasto@users.noreply.github.com>
2020-07-27 11:20:51 -0500

    Update auto_crop.py
    added custom left, right, top, and bottom padding.

--- plantcv/plantcv/auto_crop.py
+++ plantcv/plantcv/auto_crop.py
@@ -9,14 +9,16 @@ from plantcv.plantcv import params
 from plantcv.plantcv import fatal_error
 
 
-def auto_crop(img, obj, padding_x=0, padding_y=0, color='black'):
+def auto_crop_edit(img, obj, padding_t=0, padding_b=0, padding_l=0, padding_r=0 color='black'):
     """Resize image.
 
     Inputs:
     img       = RGB or grayscale image data
     obj       = contours
-    padding_x = padding in the x direction
-    padding_y = padding in the y direction
+    padding_l = padding to the left of the cropped image
+    padding_r = padding to the right of the cropped image
+    padding_t = padding to the top of the cropped image
+    padding_b = padding to the bottom of the cropped image
     color     = either 'black', 'white', or 'image'
 
     Returns:
@@ -42,19 +44,21 @@ def auto_crop(img, obj, padding_x=0, padding_y=0, color='black'):
 
     crop_img = img[y:y + h, x:x + w]
 
-    offsetx = int(np.rint(padding_x))
-    offsety = int(np.rint(padding_y))
+    offsett = int(np.rint(padding_t))
+    offsetb = int(np.rint(padding_b))
+    offsetl = int(np.rint(padding_l))
+    offsetr = int(np.rint(padding_r))
 
     if color.upper() == 'BLACK':
         colorval = (0, 0, 0)
-        cropped = cv2.copyMakeBorder(crop_img, offsety, offsety, offsetx, offsetx, cv2.BORDER_CONSTANT, value=colorval)
+        cropped = cv2.copyMakeBorder(crop_img, offsett, offsetb, offsetl, offsetr, cv2.BORDER_CONSTANT, value=colorval)
     elif color.upper() == 'WHITE':
         colorval = (255, 255, 255)
-        cropped = cv2.copyMakeBorder(crop_img, offsety, offsety, offsetx, offsetx, cv2.BORDER_CONSTANT, value=colorval)
+        cropped = cv2.copyMakeBorder(crop_img, offsett, offsetb, offsetl, offsetr, cv2.BORDER_CONSTANT, value=colorval)
     elif color.upper() == 'IMAGE':
         # Check whether the ROI is correctly bounded inside the image
-        if x - offsetx < 0 or y - offsety < 0 or x + w + offsetx > width or y + h + offsety > height:
-            cropped = img_copy2[y - offsety:y + h + offsety, x - offsetx:x + w + offsetx]
+        if x - offsetl < 0 or y - offsett < 0 or x + w + offsetl > width or y + h + offsett > height:
+            cropped = img_copy2[y - offsett:y + h + offsett, x - offsetl:x + w + offsetl]
         else:
             # If padding is the image, crop the image with a buffer rather than cropping and adding a buffer
             cropped = img_copy2[y:y + h, x:x + w]
